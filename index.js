a6029d03b0ab00ff3e9e3206c7fc0a2024d6b9c2
ayushtrapasiya <ayushtrapasiya@gmail.com>
2024-01-02 20:02:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var loader = document.getElementById("preloder");
+
+window.addEventListener("load", function () {
+
+  loader.style.display = "none"
+})
+
+
+const one = document.getElementById("value");
+animateValue(one, 0, 231, 5000);
+function animateValue(one, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    one.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+
+const two = document.getElementById("value2");
+animateValue(two, 0, 520, 5000);
+function animateValue(two, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    two.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+const three = document.getElementById("value3");
+animateValue(three, 0, 1420, 5000);
+function animateValue(three, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    three.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+const four = document.getElementById("value4");
+animateValue(four, 0, 31, 5000);
+function animateValue(four, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    four.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+
+
